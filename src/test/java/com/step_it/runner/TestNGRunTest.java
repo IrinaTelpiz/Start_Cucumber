588cec4796090e5e0e21faabe795071e6faab1c6
IrinaTelpiz <irinarelpiz@gmail.com>
2023-08-14 18:50:28 +0300

    added pom gitignore runner

---
+++ src/test/java/com/step_it/runner/TestNGRunTest.java
@@ -0,0 +1,20 @@
+package com.step_it.runner;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+        features = {"src/test/resources/features/checkout"},
+        glue = {"com.step_it.step_definitions", "com.step_it.hooks"},
+        monochrome = false,
+        tags = "",
+        plugin = {"pretty",
+                "json:test-output/cucumber-reports/Cucumber.json",
+                "junit:test-output/cucumber-reports/Cucumber.xml",
+                "html:test-output/cucumber-reports/Cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        }
+)
+
+public class TestNGRunTest extends AbstractTestNGCucumberTests {
+}
